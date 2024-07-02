5976dd27392de5d233df9450d0c6af8f9dbfd4d3
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-07-02 17:19:26 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeDateTime/ExercicioDateTime.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeDateTime
+{
+    public static class ExercicioDateTime
+    {
+        public static void CalcularIdade()
+        {
+            Console.WriteLine("Digite sua data de nacimento");
+            string data = Console.ReadLine();
+            DateTime data2 = DateTime.Parse(data);
+
+            int idade = DateTime.Now.Year - data2.Year;
+
+            if (DateTime.Now.Month < data2.Month)
+            {
+                idade = idade - 1;
+            }
+            else if (DateTime.Now.Month == data2.Month
+                 && DateTime.Now.Day < data2.Day)
+            {
+                idade = idade - 1;
+            }
+
+            Console.WriteLine(idade);
+        }
+
+        public static void VerificaAniversario()
+        {
+            Console.WriteLine("Digite sua data de nacimento");
+            string data = Console.ReadLine();
+            DateTime data2 = DateTime.Parse(data);
+
+            if(data2.Month == DateTime.Now.Month
+               && data2.Day == DateTime.Now.Day){
+                Console.WriteLine("Parabéns é seu aniversário!!");
+            }
+            else
+            {
+                Console.WriteLine("Não é seu aniversário!!");
+            }
+        }
+        public static void FormatarDataHora()
+        {
+            Console.WriteLine("Digite sua data de nacimento");
+            string data = Console.ReadLine();
+            DateTime data2 = DateTime.Parse(data);
+            Console.WriteLine(data2.ToString("dd/MM/yyyy HH:mm"));
+        }
+    }
+}
---
+++ AtividadeDateTime/Program.cs
@@ -0,0 +1,53 @@
+﻿using AtividadeDateTime;
+
+//ExercicioDateTime.CalcularIdade();
+//ExercicioDateTime.VerificaAniversario();
+ExercicioDateTime.FormatarDataHora();
+
+//DateTime data1 = new DateTime(2024, 10, 07, 8, 30, 25);
+//Console.WriteLine(data1);
+//Console.WriteLine(data1.ToString());
+//Console.WriteLine(data1.ToString("g"));
+//Console.WriteLine(DateTime.Now.ToString("G"));
+//Console.WriteLine(data1.ToString("d"));
+//Console.WriteLine(data1.ToString("D"));
+//Console.WriteLine(data1.ToString("t"));
+//Console.WriteLine(data1.ToString("T"));
+//Console.WriteLine(data1.ToString("T"));
+//Console.WriteLine(data1.ToString("yyyy-MM-dd"));
+
+//string data = "2024-07-01";
+//DateTime data2 = DateTime.Parse(data);
+//Console.WriteLine(data2);
+
+//DateTime data3 = DateTime.Now;
+//Console.WriteLine(data3);
+//Console.WriteLine(data3.Year);
+//Console.WriteLine(data3.Month);
+//Console.WriteLine(data3.Day);
+//Console.WriteLine(data3.Hour);
+//Console.WriteLine(data3.Minute);
+//Console.WriteLine(data3.Second);
+
+//DateTime hoje = DateTime.Now;
+//DateTime dataDevolucao = hoje.AddDays(7);
+//dataDevolucao = dataDevolucao.AddHours(2);
+
+//Console.WriteLine($"Você pegou o livro no dia {hoje} \n " +
+//                  $" deverá devolver no dia {dataDevolucao} ");
+
+//TimeSpan resultado = dataDevolucao - hoje;
+//TimeSpan diferenca = hoje.Subtract(dataDevolucao);
+
+//Console.WriteLine(resultado);
+//Console.WriteLine(resultado.Days);
+//Console.WriteLine(resultado.Hours);
+//Console.WriteLine(diferenca);
+//Console.WriteLine(diferenca.Days);
+//Console.WriteLine(resultado.Hours);
+//Console.WriteLine(resultado.TotalHours);
+//Console.WriteLine(resultado.TotalSeconds);
+
+
+
+
